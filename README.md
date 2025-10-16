df017425d2ff4710f2147716a9d42879558d2fb8
PC <imanpicture90@gmail.com>
2025-10-16 20:37:02 +0200

    various project to build logic and problem solving

---
+++ README.md
@@ -0,0 +1,101 @@
+🖥 Project One – Computer Service Checker
+
+This beginner Python program checks if the user’s city is within the available service regions and verifies if they are old enough to drive based on their age and driving license status.
+
+
+---
+
+🎨 Project Two – Favorite Colors & Class Lists
+
+This project asks users for their favorite colors and combines two class lists using Python list operations like append() and extend().
+
+
+---
+
+🔐 Project Three – PIN Verification
+
+A simple program that generates a random 4-digit PIN and compares it with the user’s input to check if it matches.
+
+
+---
+
+💸 Project Four – Whose Wallet?
+
+A fun game where the program randomly selects one person from a list of names to pay for dinner. It uses the random module and list manipulation.
+
+
+---
+
+🧺 Project Five – Basket Organizer
+
+Demonstrates list operations such as insert(), remove(), and append() to modify and organize a basket list.
+
+
+---
+
+🐇 Project Six – Place the Rabbit
+
+A mini interactive game where the user chooses a position (row and column) to place a rabbit 🐇 on a 3x3 grid.
+
+
+---
+
+🌻 Project Seven – Rabbit on the Field
+
+A similar concept to Project Six but with sunflowers 🌻 and different grid structure — enhances understanding of nested lists.
+
+
+---
+
+💰 Project Eight – Find the Treasure
+
+A guessing game where the player tries to find the hidden treasure 💰 on a 5x5 grid until they guess the correct position.
+
+
+---
+
+👥 Project Nine – Abbreviated Names (Method 1)
+
+This program takes a list of full names and creates abbreviations from the first and last initials using loops and string methods.
+
+
+---
+
+🔠 Project Ten – Abbreviated Names (Method 2)
+
+Another version that extracts the last letters from the first and last names, displaying clean output formatting.
+
+
+---
+
+💬 Project Eleven – Friends Abbreviations & Counting
+
+Improved version that removes symbols, abbreviates names, and shows how many friends are in the list.
+
+
+---
+
+🔁 Project Twelve – Reverse Sentence
+
+A small project that reverses the order of words in a sentence entered by the user.
+
+
+---
+
+✂ Project Thirteen – Remove Punctuation
+
+Removes punctuation marks from a given sentence using the string module, showing basic text cleaning.
+
+
+---
+
+➖ Project Fourteen – Negative Number Calculator
+
+Takes a list of numbers and subtracts them from a given number, displaying results interactively.
+
+
+---
+
+🎯 Project Fifteen – Guess the Number
+
+A guessing game where the player must guess a random number between 1 and 10, receiving hints if it’s too high or too low.
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#Projet one
 print('hello in our application')
 city=input('please type your area to see if we can help you fix your computer in our selected regions')
 city=city.strip().lower()
@@ -24,16 +25,206 @@ elif age>=18 and prm=='no':
 else:
     print ( 'it is not logical to get perme and you are jut '+str(age) +' years old ' )
 
+#Project two:
+
+color=input('enter your first color that you really like ').lower()
+more= input('do you want to add more colors : Yes or No?').lower()
+if more=='yes':
+    add_color=input("add another color to the list :").lower()
+    colors=[]
+    colors.append(color)
+    colors.append(add_color)
+    print(f'''the colors you like are:
+{colors}''')
+elif more=='no':
+    print(f'your favorite color is {color}')
+else:
+    print('invalid option')
+
+
+class_a=['ahmed','rim','sami','abd']
+class_b=['rom','hani','sara']
+all_courses=class_a+class_b
+print(all_courses)
+
+
+all=[]
+all.extend(class_a)
+all.extend(class_b)
+print(all)
+
+#Project three
+import random
+pin_ran=random.randint(1000,9999)
+pin_if=input('enter 4 digits PIN code')
+if pin_if==pin_ran:
+    print('congratulations your code is correct')
+elif len(pin_if)!=4:
+    print('please enter 4 digits pin code')
+elif len(pin_if)==4 and pin_if!=pin_ran:
+    print(f'failure you entered this {pin_if } but the computer made this code {pin_ran}')
+else:
+    print ('invalid option')
+
+#project four
+import random
+print('Welcome to whose wallet')
+print('You will give me a list of names , and i will pick a person to pay')
+names=input('If you get ready ,enter the names separated by a comma ')
+list_name=names.split(', ')
+random_list=random.randint(0,len(list_name)-1)
+final_name=list_name[random_list]
+print(f'please ask {final_name} to take his wallet out .DINNER ON HIM')
+
+#two ways to solve
+
+import random
+names=input('''Welcome to whose wallet
+You will give me a list of names , and i will pick a person to pay
+If you get ready ,enter the names separated by a comma ''').split(', ')
+print(f'please ask {random.choice(names)}to take his wallet out .DINNER ON HIM')
+
+
+#project five:
+basket=[['apples','bananas'],
+
+        ['milk','water']]
+print(basket)
+enter=input('Press enter to change the context of the list ......')
+basket[0].insert(0,'Oranges')
+basket[0].insert(3,'Kiwis')
+basket[1].remove('water')
+basket[1].insert(0,'Coffee')
+basket[1].append('tea')
+basket.append(['1', '2','3'])
+print(basket)
+
+#project six:
+print('welcome to place the rabbit')
+list_one=[['☘','☘','☘'],['☘','☘','☘'],['☘','☘','☘']]
+print(f'{list_one[0]}\n {list_one[1]}\n {list_one[2]}')
+po=input('where should the rabbit go')
+number_one=int(po[0])-1
+number_two=int(po[1])-1
+list_one[number_one][number_two]='🐇'
+print(f'Success.....\n{list_one[0]}\n {list_one[1]}\n {list_one[2]}')
+
+#projet seven:
+
+print('welcome to the place of the rabbit')
+way=['🌻','🌻','🌻'],['🌻','🌻','🌻'],['🌻','🌻','🌻']
+print(f'{way[0]}\n{way[1]}\n{way[2]}')
+go=input('''where should the rabbit go?
+Please choose a row and a column''')
+row=int(go[0])-1
+column=int(go[1])-1
+way[row][column]='🐇'
+print(f'{way[0]}\n{way[1]}\n{way[2]}')
+
+#project eight:
+
+print('welcome to our stone iland')
+stone=['💀','💀','💀','💀','💀'],['💀','💀','💀','💀','💀'],['💀','💀','💀','💀','💀'],['💀','💀','💀','💀','💀'],['💀','💀','💀','💀','💀']
+print(f'{stone[0]}\n{stone[1]}\n{stone[2]}\n{stone[3]}\n{stone[4]}')
+row_one=3
+column_one=4
+hide=input('''guess where is the money?
+Please choose a row and a column''')
+row=int(hide[0])-1
+column=int(hide[1])-1
+stone[row][column]='💰'
+
+while row!=row_one or column!=column_one:
+    print(f'''that is false :{hide}
+try again''')
+    hide = input('''guess where is the money?
+    Please choose a row and a column''')
+print(f'''congrats! you found the money 🤑
+{stone[0]}\n{stone[1]}\n{stone[2]}\n{stone[3]}\n{stone[4]}''')
+
+#Projet nine:
+name=input ('Enter the first name and the last name of your friends separated by a comma').split(',')
+listy=[]
+for i in name:
+    lists=i.split()
+    print(lists)
+    join='.'.join([lists[0][0].upper(),lists[-1][0].upper()])+'.'
+    listy.append(join)
+print('Abbreviated Names:')
+print('\n'.join(listy))
+#project ten:
+
+# التمرين الأول: آخر حرف من الاسم الأول + الأخير
+nom = input('Enter the first name of your friends separated by a comma: ').split(',')
+letters = []
+
+for name in nom:
+    name = name.strip()  # إزالة المسافات الزائدة من البداية والنهاية
+    words = name.split()
+    if len(words) >= 2:  # للتأكد من وجود اسم أول واسم أخير
+        abbrev = '.'.join([words[0][-1], words[-1][-1]])
+        letters.append(abbrev)
+
+print('Abbreviated Names:')
+print('\n'.join(letters))
+
+#Projet eleven:
+# التمرين الثاني: أول حرف من الاسم الأول + أول حرف من الاسم الأخير + عدد الأصدقاء
+nom_two = input('Enter the first name of your friends separated by a comma: ').split(',')
+letters_two = []
+
+for name in nom_two:
+    name = name.strip(' ,!.@#$%^&*()_-+=?/\"')  # تنظيف الرموز من البداية والنهاية
+    words = name.split()
+    if len(words) >= 2:
+        abbrev = '.'.join([words[0][0], words[-1][0]])
+        letters_two.append(abbrev)
+
+print('Abbreviated Names:')
+print('\n'.join(letters_two))
+
+numbers = len(nom_two)
+print(f'You have {numbers} friends in your list')
+
+
+#projet twelve:
+test=input('enter a sentence:').split()
+opposite=test[-1:-1000000:-1]
+join=' '.join(opposite)
+print(f' reversed sentence: {join}')
 
 
 
+#project thirteen:
+import string
+sentence=input("Enter a sentence: ")
+r=''
+for word in sentence:
+    if word not in string.punctuation:
+        r+=word
+print(r)
 
+#Project fourteen:
+print('hello world')
+numbers=input('enter numbers separated by comma').split(',')
+number=int(input('which number you want to negative ?'))
+for i in numbers:
+    number-=int(i)
+    print(i)
+    print(f' is {i}')
 
-
-
-
-
-
+#Project fifteen
+import random
+number_1=int(input('Guess a number between 1 and 10: '))
+secret_number=random.randint(1,10)
+while number_1 != secret_number:
+    if  number_1 < secret_number:
+        number_1=int(input('That is too low! Guess again: '))
+    elif number_1>secret_number:
+        number_1=int(input('That is too high! Guess again: '))
+    else:
+        number_1=int(input('invalid input! Please Guess a number between 1 and 10: : '))
+print(f'You guessed the secret number {secret_number}!')
 
 
 
